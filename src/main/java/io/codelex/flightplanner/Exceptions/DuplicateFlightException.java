167f49f55ba184e67a9e1f5461aecebb03558c31
SunPower120 <112656928+SunPower120@users.noreply.github.com>
2023-07-14 10:38:42 +0300

    CleanStart

--- src/main/java/io/codelex/flightplanner/Exceptions/DuplicateFlightException.java
+++
@@ -1,7 +0,0 @@
-package io.codelex.flightplanner.Exceptions;
-
-public class DuplicateFlightException extends Exception {
-    public DuplicateFlightException(String message) {
-        super(message);
-    }
-}
